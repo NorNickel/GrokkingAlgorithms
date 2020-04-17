93f5831a4f7639d7d4ebe3e42d4f129342439e28
NorNickel <aaaxxxspb@gmail.com>
2020-04-17 23:42:40 +0300

    quick sorting realization

---
+++ src/main/java/best/aog/QuickSort.java
@@ -0,0 +1,44 @@
+package best.aog;
+
+public class QuickSort {
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0 || low >= high) {
+            return;
+        }
+        
+        // выбрать опорный элемент
+        int middle = low + (high - low) / 2;
+        int pivot = array[middle];
+
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {//меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+}
